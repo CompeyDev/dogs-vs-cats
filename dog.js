e02e513e8a03c48bbe4888ef90e30bf037e93d23
DevComp <hi@devcomp.tk>
2022-03-26 05:56:03 +0000

    🎉Initialize!

---
+++ dog.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const express = require('express');
+const app = express();
+let img;
+
+app.listen(3000, () => console.log("Server listening on port 3000!"));
+
+/** app.get("/", (req, res) => res.send(`<head><meta property="og:image" content="${img}"></head>`)); */
+
+
+app.get("/", (req, res) => res.send(`<html><head><meta property="og:image" content="${img}"></head><body><img src='${img}' width='1000'></img></body></html>`));
+
+async function fetch(){
+  const res = await axios.get('https://random.dog/woof.json');
+  img = res.data.url;
+  console.log("Fetched new cat from", img)
+}
+
+setInterval(fetch, 1000);
